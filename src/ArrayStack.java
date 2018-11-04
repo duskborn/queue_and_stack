897a95752a70c672399e097da75710a53ac2cc9f
mozhg <mozhgru@gmail.com>
2018-11-04 18:37:14 +0400

    ArrayStack: i variable deleted

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -3,18 +3,15 @@ import java.util.ArrayList;
 public class ArrayStack<T> implements Stack<T> {
 
     private ArrayList<T> stack = new ArrayList();
-    int i = 0;
 
     @Override
     public void push(T value) {
         stack.add(value);
-        i++;
     }
 
     @Override
     public T pop() {
-        i--;
-        return stack.remove(i);
+        return stack.remove(stack.size()-1);
     }
 
     @Override
